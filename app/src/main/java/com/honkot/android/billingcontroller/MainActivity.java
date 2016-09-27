640f1e1ddb1c3c9f50f96f546726c82ddfd6dd0d
本田裕規 <hon87kot12.xp@gmail.com>
2016-09-26 17:55:58 -0700

    Add button and log view.

--- app/src/main/java/com/honkot/android/billingcontroller/MainActivity.java
+++ app/src/main/java/com/honkot/android/billingcontroller/MainActivity.java
@@ -2,12 +2,50 @@ package com.honkot.android.billingcontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Textlog mLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLog = new Textlog((TextView)findViewById(R.id.log));
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                break;
+            case R.id.button2:
+                break;
+            case R.id.button3:
+                break;
+        }
+    }
+
+    private class Textlog {
+        TextView mLogView;
+        Calendar mCal;
+
+        Textlog(TextView tv) {
+            mCal = Calendar.getInstance();
+            mLogView = tv;
+        }
+
+        public void print(String log) {
+            mCal.setTimeInMillis(System.currentTimeMillis());
+            StringBuffer buf = new StringBuffer();
+            buf.append(log);
+            buf.append(System.getProperty("line.separator"));
+            buf.append(mLogView.getText());
+            mLogView.setText(buf.toString());
+        }
     }
 }
